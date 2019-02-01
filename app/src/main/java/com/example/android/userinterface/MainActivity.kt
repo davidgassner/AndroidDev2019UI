d4403423de2eaffa0f6a3f2033280c8e22dbc23c
David Gassner <dgassnerdev@gmail.com>
2019-02-01 13:52:47 -0800

    04_04e

--- app/src/main/java/com/example/android/userinterface/MainActivity.kt
+++ app/src/main/java/com/example/android/userinterface/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.android.userinterface
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,21 +14,28 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         button1.setOnClickListener {
-            displayImage(R.drawable.monster01)
+            displayImageAsset("monster01.webp")
         }
 
         button2.setOnClickListener {
-            displayImage(R.drawable.monster02)
+            displayImageAsset("monster02.webp")
         }
 
         button3.setOnClickListener {
-            displayImage(R.drawable.monster03)
+            displayImageAsset("monster03.webp")
         }
 
     }
 
-    private fun displayImage(resId: Int) {
-        monsterImage.setImageResource(resId)
+    private fun displayImageAsset(fileName: String) {
+        assets.open(fileName).use {
+            val drawable = Drawable.createFromStream(it, null)
+            monsterImage.setImageDrawable(drawable)
+        }
     }
+
+//    private fun displayImage(resId: Int) {
+//        monsterImage.setImageResource(resId)
+//    }
 
 }
