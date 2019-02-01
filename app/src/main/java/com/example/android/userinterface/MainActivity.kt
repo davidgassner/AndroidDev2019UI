def15a48b051c954fc7914e2f15ae6bf216e20e0
David Gassner <dgassnerdev@gmail.com>
2019-02-01 11:10:33 -0800

    03_02e

--- app/src/main/java/com/example/android/userinterface/MainActivity.kt
+++ app/src/main/java/com/example/android/userinterface/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.android.userinterface
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +12,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        loginButton.setOnClickListener{
+            val message = "Your name is ${nameInput.text} " +
+                    "and your password is ${passwordInput.text}"
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+        }
     }
 
 }
