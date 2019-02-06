1802e1f09095d82107fdbad869238f91c1ffdcbd
David Gassner <dgassnerdev@gmail.com>
2019-02-06 12:53:05 -0800

    02_05e

--- app/src/main/java/com/example/android/userinterface/MainActivity.kt
+++ app/src/main/java/com/example/android/userinterface/MainActivity.kt
@@ -2,17 +2,21 @@ package com.example.android.userinterface
 
 import android.graphics.Color
 import android.os.Bundle
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var container: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        container = findViewById(R.id.linearLayout)
 
         addTextView("Hello")
         addTextView("From")
@@ -24,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         view.text = label
         view.textSize = 28f
         view.setTextColor(Color.parseColor("#ff0000"))
-        linearLayout.addView(view)
+        container.addView(view)
     }
 
 }
